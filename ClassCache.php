65f4f9e55ef4d22f781b2ee07b388487a3d28ff8
Shingo Fukuyama <fukuyama012@gmail.com>
2018-06-03 16:28:25 +0900

    add ClassCache

---
+++ ClassCache.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * class-cache
+ * Source available at https://github.com/fukuyama012/class-cache
+ * This is distributed under the terms of the MIT license.
+ */
+abstract class ClassCache
+{
+    /** @var array */
+    private static $m_cache = [];
+
+    /**
+     * @param mixed $key
+     * @return mixed cache data
+     */
+    protected static function _cacheGet($key)
+    {
+        $key = self::_serializeKey($key);
+        if(isset(self::$m_cache[$key]))
+        {
+            return self::$m_cache[$key];
+        }
+        return null;
+    }
+
+    /**
+     * @param mixed $key
+     * @param mixed $data data for cache
+     */
+    protected static function _cacheSet($key, $data)
+    {
+        self::$m_cache[self::_serializeKey($key)] = $data;
+    }
+
+    /**
+     * @param mixed $key
+     */
+    protected static function _cacheClear($key)
+    {
+        unset(self::$m_cache[self::_serializeKey($key)]);
+    }
+
+    /**
+     * @param mixed $key
+     * @return string
+     */
+    private static function _serializeKey($key)
+    {
+        if(!is_array($key))
+        {
+            $key = array("id" => $key);
+        }
+        return sprintf("%s:%s", get_called_class(), http_build_query($key));
+    }
+}
